b962443698677867168d525b9d9ef9a9c5356e12
Daniel <satelerd@gmail.com>
2022-09-28 22:06:57 -0300

    loop for generations

--- index.py
+++ index.py
@@ -10,6 +10,8 @@ Numbers = [C1, C2]
 lenMatrix = 10  # min 2
 maxIndex = lenMatrix - 1
 zeroMatrix = np.zeros((lenMatrix, lenMatrix))
+lenGenerations = 10
+matrixHistory = []
 
 
 # Start of the simulation
@@ -19,17 +21,27 @@ print("Initial Matrix")
 print(matrix)
 print()
 
+# Main loop for each generation
+for generation in range(0, lenGenerations):
+    if generation == 0:
+        auxMatrix = matrix.copy()
 
-# Fitness for each value of the matrix
-auxMatrix = matrix.copy()
-for i in range(0, lenMatrix):  # Matrix loop
-    for j in range(0, lenMatrix):
-        neighbors = countFitness(matrix, maxIndex, i, j)
-        # neighbors=np.array(neighbors)
-        auxMatrix[i, j] = gridCellFitness(neighbors)
-        # print(gridCellFitness(neighbors))
+    else:
+        auxMatrix = matrixHistory[generation - 1].copy()
+
+    for i in range(0, lenMatrix):  # Matrix loop
+        for j in range(0, lenMatrix):
+            neighbors = countFitness(matrix, maxIndex, i, j)  # Fitness for the cell
+            auxMatrix[i, j] = gridCellFitness(neighbors)
+
+    matrixHistory.append(auxMatrix)
 
 print("Fitness Matrix")
 print(auxMatrix)
 # print(matrix)
 print()
+
+print("Matrix History")
+for i in range(0, lenGenerations):
+    print(matrixHistory[i])
+    print()
